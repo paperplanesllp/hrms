b4f88c6f61f7d4dcb0ede5348604059a3f57a036
paperplanesllp <hello@paperplanesco.com>
2026-04-02 11:56:24 +0530

    Update email transporter authentication variables

--- server/src/services/emailNotification.service.js
+++ server/src/services/emailNotification.service.js
@@ -4,8 +4,8 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   service: process.env.EMAIL_SERVICE || "gmail",
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
   }
 });
 
